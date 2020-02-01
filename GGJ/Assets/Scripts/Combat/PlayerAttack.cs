f64ebf87da6ee28f81628dc81a8fd7b27b711d00
CarlaCiurea <carla.ciurea@yahoo.com>
2020-02-01 17:51:22 +0000

    Added a weapon and a bullet

---
+++ GGJ/Assets/Scripts/Combat/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawner;
+    [SerializeField] float bulletSpeed = 10f;
+
+    bool fired = false;
+    Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Fire1") != 0)
+        {
+            if (fired == false)
+            {
+                myAnimator.SetTrigger("Fire");
+                fired = true;
+            }
+        }
+        if (Input.GetAxisRaw("Fire1") == 0)
+        {
+            fired = false;
+        }
+    }
+
+    void Hit()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawner.position, bulletSpawner.rotation);
+        bullet.transform.parent = bulletSpawner.transform;
+        bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+    }
+}
