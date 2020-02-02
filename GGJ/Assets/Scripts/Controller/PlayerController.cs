1260cfa0cb9d7da537acf7558ea6bdd8854a3ab0
kate <41784325+D0A9F4@users.noreply.github.com>
2020-02-02 16:28:54 +0000

    Fixed Bug

--- GGJ/Assets/Scripts/Controller/PlayerController.cs
+++ GGJ/Assets/Scripts/Controller/PlayerController.cs
@@ -7,11 +7,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 5f;
 
     float vertical;
-    // Start is called before the first frame update
-    void Start()
-    {
-        myAnimator = GetComponent<Animator>();
-    }
 
     // Update is called once per frame
     void Update()
