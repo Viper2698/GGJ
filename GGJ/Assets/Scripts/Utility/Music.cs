48c12c1becff2227d234838eab542e0301760a91
Rita <rita.martinif@gmail.com>
2020-02-02 14:49:54 +0000

    added music transition between scenes

---
+++ GGJ/Assets/Scripts/Utility/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (FindObjectsOfType<Music>().Length > 1)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
